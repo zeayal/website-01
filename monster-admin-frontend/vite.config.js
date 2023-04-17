11a0ab7b7177fff5ff1f1fa63f90ffac1a25579e
ziyang.liu <zeayalsir@gmail.com>
2023-04-17 13:21:53 +0800

    [添加dockerfile]

--- monster-admin-frontend/vite.config.js
+++ monster-admin-frontend/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/monster-admin-frontend/",
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: mapToken,
-        javascriptEnabled: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
